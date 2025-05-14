deb2390e83248ecac622e323c1a7d27436badbbb
lucas <accaunoff99@gmail.com>
2025-05-14 17:49:49 +0900

    FEAT: started x-clone and builded auth page UI

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,17 @@
+
+import Auth from '@/components/auth'
+import React from 'react'
+
+export default function Page() {
+
+  const user = false
+
+
+    if(!user) return (
+      <div className='container h-screen mx-auto max-w-7xl'>
+      <Auth/>
+      </div>
+    )
+
+
+}
