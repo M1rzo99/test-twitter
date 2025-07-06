7bacbbacb5a5a4936d973c3277e8f905011ffb70
Mirzo <mirzo@Mirzoui-MacBookPro.local>
2025-07-06 11:16:03 +0900

    FEAT: making Followers/Following

--- components/profile/profile-bio.tsx
+++ components/profile/profile-bio.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { IUser } from "@/types"
 import { useState } from "react";
 import {IoLocationSharp} from "react-icons/io5"
@@ -9,11 +10,23 @@ import { useRouter } from "next/navigation";
 import Button from "../ui/button";
 import EditModal from "../modals/edit-modal";
 import useEditModal from "@/hooks/useEditModal";
+import Modal from "../ui/modal";
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import User from "../shared/user";
+
 
 const ProfileBio=({user,userId}:{user:IUser; userId:string})=>{
     const [isLoading,setIsLoading] = useState(false)
+    const [Open,setOpen] = useState(false)
+    const [following,setFollowing] = useState<IUser[]>([])
+    const [followers,setFollowers] = useState<IUser[]>([])
+    const [isFetching,setIsFetching] = useState(false)
+    const [state, setState] = useState <"following" | "followers">("followers")
+
     const router = useRouter()
     const editModal = useEditModal()
+
 
     const onFollow =async ()=>{
        try {
@@ -38,6 +51,46 @@ const ProfileBio=({user,userId}:{user:IUser; userId:string})=>{
         setIsLoading(false)
        }  
     }
+
+    const getFollowUsers = async (userId:string,type:string)=>{
+        try {
+            setIsFetching(true)
+            const {data} = await axios.get(`/api/follows?state=${type}&userId=${userId}`)
+            setIsFetching(false)
+           return data
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+       const OpenFollowModal = async ()=>{
+        try {
+            setOpen(true)
+            const data = await getFollowUsers(user._id,"following")
+            setFollowing(data)
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    const onFollowing = async ()=>{
+        setState('following')
+        if(following.length === 0){
+            const data = await getFollowUsers(user._id,"following")
+            setFollowing(data)
+        }
+    }
+
+    const onFollowers = async ()=>{
+        setState('followers')
+        if(followers.length === 0){
+            const data = await getFollowUsers(user._id,"followers")
+            setFollowers(data)
+        }
+    }
+
     return (
     <>
     <EditModal user={user} />
@@ -71,12 +124,12 @@ const ProfileBio=({user,userId}:{user:IUser; userId:string})=>{
                     </div>
                    </div>
                    <div className="flex flex-row items-center mt-6 gap-6">
-                    <div className="flex flex-row items-center gap-1 hover:underline cursor-pointer">
+                    <div className="flex flex-row items-center gap-1 hover:underline cursor-pointer" onClick={OpenFollowModal}> 
                         <p className="text-white">{user.following}</p>
                         <p className="text-neutral-500">Following</p>
                     </div>
 
-                    <div className="flex flex-row items-center gap-1 hover:underline cursor-pointer">
+                    <div className="flex flex-row items-center gap-1 hover:underline cursor-pointer"  onClick={OpenFollowModal}> 
                         <p className="text-white">{user.followers}</p>
                         <p className="text-neutral-500">Followers</p>
                     </div>
@@ -85,6 +138,50 @@ const ProfileBio=({user,userId}:{user:IUser; userId:string})=>{
             </div>
         </div>
     </div>
+    {/* Following and Followers Modal */}
+
+                    <Modal  isOpen={Open} onClose={()=>setOpen(false)} body={
+                        <>
+                        <div className="flex flex-row w-full py-3 px-4">
+                            
+                            <div className={cn(
+                                "w-[50%] h-full flex justify-center items-center cursor-pointer font-semibold", state === "following" && "border-b-[2px] border-sky-500 text-sky-500"
+                            )}  onClick={onFollowing}> 
+                           
+                                Following
+                            </div>
+                            
+                            <div className={cn(
+                                "w-[50%] h-full flex justify-center items-center cursor-pointer font-semibold", state === "followers" && "border-b-[2px] border-sky-500 text-sky-500"
+                            )} onClick={onFollowers}> 
+                                Followers
+                            </div>
+                        </div>
+
+                        {isFetching ? (
+                            <div className="flex justify-center items-center h-24">
+                                <Loader2 className="animate-spin text-sky-500"/>
+                            </div>
+                        ) : (
+                            <div className="space-y-4 flex flex-col">
+                               { state === "following" ? (
+                                following.length === 0 ? (
+                                    <div className="text-neutral-600 text-center p-6 text-xl">No Following</div>
+                                ) : (
+                                    following.map((user) => <User key={user._id} user={user} />)
+                                )
+                               ) : (
+
+                              followers.length === 0 ? (
+                                    <div className="text-neutral-600 text-center p-6 text-xl">No Followers</div>
+                                ) : (
+                                    followers.map((user) => <User key={user._id} user={user} />)
+                                )
+                            )}
+                            </div>
+                        )}
+                        </>
+                    }/>
     </>)
 }
-export default ProfileBio
+export default ProfileBio 
