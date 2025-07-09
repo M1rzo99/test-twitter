0a267176cb5159c1a8c396b4a50dd5ea2a99824c
Mirzo <mirzo@example.com>
2025-07-10 07:54:20 +0900

    FEAT: making Notification section

--- app/(root)/(home)/notifications/[userId]/page.tsx
+++ app/(root)/(home)/notifications/[userId]/page.tsx
@@ -1,9 +1,25 @@
+"use client"
 
+import Header from '@/components/shared/header'
+import useNotifications from '@/hooks/useNotifications'
+import { Loader2 } from 'lucide-react'
 import React from 'react'
 
 const NotificationPage = ({params}:{params:{userId:string}}) => {
+    const {data,isLoading,mutate} = useNotifications(params.userId)
+    console.log(data);
+
   return (
-    <div>NotificationPage</div>
+<>
+<Header isBack label='Notifications'/>
+{isLoading ? (
+<div className='flex items-center justify-center h-24'>
+  <Loader2 className='animate-spin text-sky-500'/>
+</div>
+) : (
+  <></>
+)}
+</>
   )
 }
 
